e2a489c13351d798c23f8deba0317f76da755784
Fred B <fredbitenyo@gmail.com>
2023-05-08 22:19:00 +0200

    added vpc base code

--- main.tf
+++ main.tf
@@ -7,7 +7,9 @@ locals {
 # create vpc module
 module "vpc" {
   source                       = "git@github.com:Hirodari/rentzone-terraform-modules-ecs.git//vpc"
-  project_name                 = var.project
+  project_name                 = local.project
+  environment                  = local.environment
+  region                       = local.region
   vpc_cidr                     = var.vpc_cidr
   public_subnet_az1_cidr       = var.public_subnet_az1_cidr
   public_subnet_az2_cidr       = var.public_subnet_az2_cidr
