644f4ccdcada990605680b35c95a2971c1f92415
Fred B <fredbitenyo@gmail.com>
2023-05-09 00:58:46 +0200

    rds code base added

--- main.tf
+++ main.tf
@@ -18,3 +18,42 @@ module "vpc" {
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
+
+# create natgateway module
+module "natgateway" {
+  source                     = "git@github.com:Hirodari/rentzone-terraform-modules-ecs.git//natgateway"
+  project_name               = local.project
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+}
+
+# create security groups module
+module "security-groups" {
+  source       = "git@github.com:Hirodari/rentzone-terraform-modules-ecs.git//security-groups"
+  project_name = local.project
+  environment  = local.environment
+  vpc_id       = module.vpc.vpc_id
+  ssh_ip       = var.ssh_ip
+}
+
+# create rds module
+module "rds" {
+  source                     = "git@github.com:Hirodari/rentzone-terraform-modules-ecs.git//rds"
+  project_name               = local.project
+  environment                = local.environment
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  db_snapshot_identifier     = var.db_snapshot_identifier
+  db_instance_class          = var.db_instance_class
+  availability_zone_2        = module.vpc.availability_zone_2
+  db_instance_identifier     = var.db_instance_identifier
+  multi_az_deployment        = var.multi_az_deployment
+  database_security_group_id = module.security-groups.database_security_group_id
+}
--- variables.tf
+++ variables.tf
@@ -12,3 +12,11 @@ variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_az1_cidr" {}
 variable "private_data_subnet_az2_cidr" {}
 
+# security groups variables
+variable "ssh_ip" {}
+
+# rds variables
+variable "db_snapshot_identifier" {}
+variable "db_instance_class" {}
+variable "db_instance_identifier" {}
+variable "multi_az_deployment" {}
